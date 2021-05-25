bc1be634e0ae97cf054d6ac1d2dd54b51fa34468
Aleksandr Oksen <aledor777@gmail.com>
2021-05-25 21:34:12 +0300

    03-streams line-split-stream

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -1,14 +1,42 @@
-const stream = require('stream');
+const stream = require("stream");
+const os = require("os");
 
 class LineSplitStream extends stream.Transform {
+  #data = "";
   constructor(options) {
     super(options);
   }
 
   _transform(chunk, encoding, callback) {
+    const string = chunk.toString();
+    this.#data += string;
+
+    const arr = this.#data.split(os.EOL);
+
+    if (arr.length > 1) {
+      const lastStrPart = arr[arr.length - 1];
+
+      arr.forEach((str, idx) => {
+        if (str && idx < arr.length - 2) this.push(str);
+
+        if (str && idx === arr.length - 2) {
+          this.push(str);
+
+          if (!lastStrPart) {
+            this.#data = "";
+          } else {
+            this.#data = lastStrPart;
+          }
+        }
+      });
+    }
+
+    callback();
   }
 
   _flush(callback) {
+    this.push(this.#data);
+    callback();
   }
 }
 
