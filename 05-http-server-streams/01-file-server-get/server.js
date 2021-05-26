fff39bfdfe0912e0b13e85876d0fa80604954e0e
Aleksandr Oksen <aledor777@gmail.com>
2021-05-26 21:03:53 +0300

    update limiSizeStream task, add 05-http-server-streams => 01-file-server-get task

--- 05-http-server-streams/01-file-server-get/server.js
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -1,23 +1,35 @@
-const url = require('url');
-const http = require('http');
-const path = require('path');
-
+const url = require("url");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 const server = new http.Server();
+const { pipeline } = require("stream");
 
-server.on('request', (req, res) => {
+server.on("request", (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
 
-  const filepath = path.join(__dirname, 'files', pathname);
-
-  switch (req.method) {
-    case 'GET':
-
-      break;
-
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
+  if (pathname.split("/").length > 1) {
+    res.statusCode = 400;
+    return res.end();
   }
+
+  const filepath = path.join(__dirname, "files", pathname);
+
+  fs.access(filepath, fs.F_OK, (err) => {
+    if (err) {
+      res.statusCode = 404;
+      return res.end("File not found!");
+    }
+
+    pipeline(fs.createReadStream(filepath), res, (err) => {
+      if (err) {
+        res.statusCode = 500;
+        res.end("Not implemented");
+      } else {
+        console.log("File sent");
+      }
+    });
+  });
 });
 
 module.exports = server;
