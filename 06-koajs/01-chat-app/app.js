22a617522556a2f238c07b975972d0665d6096fd
Aleksandr Oksen <aledor777@gmail.com>
2021-06-01 20:20:22 +0300

    06-koajs

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -8,10 +8,22 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+let subscribers = {};
+
 router.get('/subscribe', async (ctx, next) => {
+  subscribers[ctx.request.query.r] = ctx;
+  await new Promise((resolve) => {});
 });
 
 router.post('/publish', async (ctx, next) => {
+  if (!ctx.request.body) return;
+  for (const id in subscribers) {
+    const ctx_ = subscribers[id];
+    ctx_.status = 200;
+    ctx_.res.end(ctx.request.body.message);
+  }
+  ctx.status = 200;
+  subscribers = {};
 });
 
 app.use(router.routes());
