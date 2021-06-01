9288cccd0187fb8eef315a1ef9e4dc26d4981aa9
Aleksandr Oksen <aledor777@gmail.com>
2021-06-01 17:29:39 +0300

    add 03-file-server-delete

--- 05-http-server-streams/01-file-server-get/server.js
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -1,35 +1,47 @@
-const url = require("url");
-const http = require("http");
-const path = require("path");
-const fs = require("fs");
+const url = require('url');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 const server = new http.Server();
-const { pipeline } = require("stream");
+const {pipeline} = require('stream');
+const fsPromises = require('fs').promises;
 
-server.on("request", (req, res) => {
+server.on('request', async (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
 
-  if (pathname.split("/").length > 1) {
-    res.statusCode = 400;
-    return res.end();
-  }
+  switch (req.method) {
+    case 'GET': {
+      if (pathname.split('/').length > 1) {
+        res.statusCode = 400;
+        return res.end();
+      }
 
-  const filepath = path.join(__dirname, "files", pathname);
+      const filepath = path.join(__dirname, 'files', pathname);
 
-  fs.access(filepath, fs.F_OK, (err) => {
-    if (err) {
-      res.statusCode = 404;
-      return res.end("File not found!");
+      try {
+        await fsPromises.open(filepath, 'r');
+      } catch (err) {
+        res.statusCode = 404;
+        return res.end('File not found');
+      }
+
+      return pipeline(fs.createReadStream(filepath), res, (err) => {
+        if (err) {
+          console.log(err);
+          res.statusCode = 500;
+          res.end('Not implemented');
+        } else {
+          res.statusCode = 200;
+          res.end();
+          console.log('File sent');
+        }
+      });
     }
 
-    pipeline(fs.createReadStream(filepath), res, (err) => {
-      if (err) {
-        res.statusCode = 500;
-        res.end("Not implemented");
-      } else {
-        console.log("File sent");
-      }
-    });
-  });
+    default:
+      res.statusCode = 500;
+      res.end('Not implemented');
+  }
 });
 
 module.exports = server;
--- 05-http-server-streams/03-file-server-delete/server.js
+++ 05-http-server-streams/03-file-server-delete/server.js
@@ -1,21 +1,35 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
-
 const server = new http.Server();
+const fsPromises = require('fs').promises;
 
-server.on('request', (req, res) => {
-  const pathname = url.parse(req.url).pathname.slice(1);
-
-  const filepath = path.join(__dirname, 'files', pathname);
+server.on('request', async (req, res) => {
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const pathname = url.pathname.slice(1);
 
   switch (req.method) {
-    case 'DELETE':
+    case 'DELETE': {
+      if (pathname.split('/').length > 1) {
+        res.statusCode = 400;
+        return res.end();
+      }
 
+      const filepath = path.join(__dirname, 'files', pathname);
+
+      try {
+        await fsPromises.open(filepath, 'r');
+      } catch (err) {
+        res.statusCode = 404;
+        return res.end('File not exist');
+      }
+      await fsPromises.unlink(filepath);
+      res.statusCode = 200;
+      res.end('Deleted');
       break;
+    }
 
     default:
-      res.statusCode = 501;
+      res.statusCode = 500;
       res.end('Not implemented');
   }
 });
