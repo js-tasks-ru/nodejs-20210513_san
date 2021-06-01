63b6391e20b5920cfcc55266823e5b4f363765f6
Aleksandr Oksen <aledor777@gmail.com>
2021-06-01 21:07:16 +0300

    check empty query and message

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -11,17 +11,26 @@ const router = new Router();
 let subscribers = {};
 
 router.get('/subscribe', async (ctx, next) => {
-  subscribers[ctx.request.query.r] = ctx;
+  const id = ctx.request.query.r || Math.random();
+
+  subscribers[id] = ctx;
+
+  ctx.req.on('close', (err) => {
+    delete subscribers[id];
+  });
+
   await new Promise((resolve) => {});
 });
 
 router.post('/publish', async (ctx, next) => {
-  if (!ctx.request.body) return;
+  if (!ctx.request.body.message) return;
+  // eslint-disable-next-line guard-for-in
   for (const id in subscribers) {
     const ctx_ = subscribers[id];
     ctx_.status = 200;
     ctx_.res.end(ctx.request.body.message);
   }
+
   ctx.status = 200;
   subscribers = {};
 });
