2421dbcbccc47fc15d27c59bdf84c36256ce37a7
Aleksandr Oksen <aledor777@gmail.com>
2021-06-10 18:14:46 +0300

    07 mongo db

--- 05-http-server-streams/02-file-server-post/server.js
+++ 05-http-server-streams/02-file-server-post/server.js
@@ -2,11 +2,8 @@ const fs = require('fs');
 const http = require('http');
 const path = require('path');
 const fsPromises = fs.promises;
-const {pipeline} = require('stream');
-const LimitSizeStream = require('./LimitSizeStream');
-const {promisify} = require('util');
-
 const server = new http.Server();
+const LimitSizeStream = require('./LimitSizeStream');
 
 server.on('request', async (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
@@ -31,39 +28,48 @@ server.on('request', async (req, res) => {
         // console.log(err);
       }
 
-      const pipelinePromise = promisify(pipeline);
+      const handleError = (err) => {
+        if (err) {
+          fs.unlink(filepath, (err) => console.log());
 
-      const limitSizeStream = new LimitSizeStream({limit: 1000000});
+          if (err.code === 'LIMIT_EXCEEDED') {
+            res.statusCode = 413;
+            req.destroy();
+            writableStream.destroy();
+          } else {
+            res.statusCode = 500;
+          }
 
-      limitSizeStream.on('error', (err) => {
-        fs.unlink(filepath, (err) => console.log());
-        res.statusCode = 413;
-        res.end();
-      });
+          return res.end();
+        }
 
-      try {
-        await pipelinePromise(
-            req,
-            limitSizeStream,
-            fs.createWriteStream(filepath, {encoding: 'utf-8'}),
-        );
         res.statusCode = 201;
         res.end();
-        break;
-      } catch (err) {
-        // if (err.code === 'LIMIT_EXCEEDED') {
-        //   res.statusCode = 413;
-        // } else {
-        //   res.statusCode = 500;
-        // }
-        fs.unlink(filepath, (err) => console.log());
+      };
 
-        if (err.code !== 'LIMIT_EXCEEDED') {
-          res.statusCode = 500;
-          res.end();
+      const writableStream = fs.createWriteStream(filepath, {
+        encoding: 'utf-8',
+        emitClose: true,
+      });
+      const limitSizeStream = new LimitSizeStream({limit: 1000000});
+
+      req.on('error', handleError);
+      req.on('close', () => {
+        console.log('req closed ===================================');
+      });
+      writableStream.on('error', handleError);
+
+      req.on('readable', function() {
+        let chunk = req.read();
+        while (null !== chunk) {
+          // console.log(`Received ${chunk.length} bytes of data.`);
+          limitSizeStream._transform(chunk, 'utf-8', handleError);
+          writableStream.write(chunk);
+          chunk = req.read();
         }
-        break;
-      }
+      });
+
+      break;
     }
 
     default:
