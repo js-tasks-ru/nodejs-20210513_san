a431debecbc6226fc9a01a9587abc3b18250a251
Aleksandr Oksen <aledor777@gmail.com>
2021-06-10 18:20:55 +0300

    07 mongo db

--- 07-mongodb-mongoose/02-rest-api/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mongodb: {
-    uri: (process.env.NODE_ENV === 'test') ?
-      'mongodb://localhost/6-module-2-task' :
-      'mongodb://127.0.0.1:27017',
-    // 'mongodb://localhost/any-shop',
-  },
-};
